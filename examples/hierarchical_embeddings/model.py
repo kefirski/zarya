9e69b17e2f9ed7625130c9fe738fb84812d2506f
gooppe <gooppe@yandex.ru>
2019-10-11 18:31:33 +0300

    Addded HierarchyEmbedding model and tests

---
+++ examples/hierarchical_embeddings/model.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+
+from zarya import nn as znn
+
+
+class HieracicalEmbeddings(nn.Module):
+    """Hierarchical Embedding Model.
+
+    Args:
+        vocab_size (int): Vocabulary size.
+        emb_size (int): Embedding size.
+        manifold (zarya.manifolds.Manifold): Hyperbolic manifold.
+    """
+
+    def __init__(self, vocab_size, emb_size, manifold):
+        super().__init__()
+
+        self.mf = manifold
+        self.embedding = znn.Embedding(vocab_size, emb_size, manifold)
+        self.objective = nn.CrossEntropyLoss()
+
+    def forward(self, succ, pred, neg):
+        """Calculate hierarchy embedding loss value.
+
+        Args:
+            succ (torch.Tensor): Successor of hierarchy pair.
+            pred (torch.Tensor): Predecessor of hierarchy pair.
+            neg (torch.Tensor): Random negative samples.
+
+        Returns:
+            torch.Tensor: Calculated hierarchy loss.
+        """
+        target = torch.zeros_like(succ)
+        succ = succ.view(-1, 1)
+        pred = pred.view(-1, 1)
+
+        union = torch.cat((succ, neg), dim=-1)
+        pred_emb = self.embedding(pred)
+        union_emb = self.embedding(union)
+        distances = -self.mf.distance(pred_emb, union_emb)
+
+        return self.objective(distances, target)
+
+    def dump(self, filename=None):
+        """Detach and return embedding weights.
+
+        Returns:
+            torch.Tensor: Embedding weights.
+        """
+        return self.embedding.weight.detach().cpu()
+
+    def renorm(self):
+        self.mf.renorm(self.embedding.weight)
---
+++ tests/examples/test_hierarchy_embeddings.py
@@ -0,0 +1,19 @@
+import pytest
+
+import torch
+
+from zarya.manifolds import PoincareBall
+from examples.hierarchical_embeddings.model import HieracicalEmbeddings
+
+torch.manual_seed(42)
+
+manifold = PoincareBall()
+
+
+def test_forward():
+    batch_size, negative_size = 8, 15
+
+    pred, succ = torch.randint(10, (2, batch_size))
+    negatives = torch.randint(10, (batch_size, negative_size))
+
+    HieracicalEmbeddings(10, 30, manifold).forward(pred, succ, negatives)
