5dd4885b45fbf599e0744760adcf0b736b5ca179
gooppe <gooppe@yandex.ru>
2019-11-19 15:46:59 +0300

    Added multi threading

--- examples/hierarchical_embeddings/train.py
+++ examples/hierarchical_embeddings/train.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import multiprocessing
 
 import numpy as np
 import torch
@@ -9,7 +10,7 @@ from tqdm import tqdm
 from dataset import HierarchicalDataset
 from model import HierarchicalEmbeddings
 from utils import setup_logging
-from zarya.manifolds import PoincareBall, LorentzManifold
+from zarya.manifolds import LorentzManifold, PoincareBall
 from zarya.optim import RSGD
 
 logger = logging.getLogger(__name__)
@@ -26,17 +27,21 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--negatives", type=int, default=10)
     parser.add_argument("--closure", action="store_true")
+    parser.add_argument("--threads", type=int, default=multiprocessing.cpu_count())
     parser.add_argument(
         "--manifold", choices=["poincare", "lorentz"], default="poincare"
     )
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.set_num_threads(args.threads)
 
     dataset = HierarchicalDataset(
         args.source, args.vocab, args.negatives, closure=args.closure
     )
-    dataloader = DataLoader(dataset, args.bs, True, collate_fn=dataset.collate)
+    dataloader = DataLoader(
+        dataset, args.bs, True, collate_fn=dataset.collate, num_workers=args.threads
+    )
     pbar = tqdm(total=len(dataloader) * args.epochs, desc="Trainig")
 
     manifold = PoincareBall() if args.manifold == "poincare" else LorentzManifold()
