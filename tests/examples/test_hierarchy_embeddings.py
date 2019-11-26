d54aa1cc0a030049abe07101b5e40389c7ccfcac
gooppe <gooppe@yandex.ru>
2019-11-26 19:32:34 +0300

    Added entailment tests

--- tests/examples/test_hierarchy_embeddings.py
+++ tests/examples/test_hierarchy_embeddings.py
@@ -1,7 +1,10 @@
 import torch
 
-from zarya.manifolds import PoincareBall
-from examples.hierarchical_embeddings.model import HierarchicalEmbeddings
+from zarya.manifolds import PoincareBall, LorentzManifold
+from examples.hierarchical_embeddings.model import (
+    HierarchicalEmbeddings,
+    EntailmentConesEmbeddings,
+)
 
 torch.manual_seed(42)
 
@@ -15,3 +18,25 @@ def test_forward():
     negatives = torch.randint(10, (batch_size, negative_size))
 
     HierarchicalEmbeddings(10, 30, manifold).forward(pred, succ, negatives)
+
+
+def test_entcon_poincare_forward():
+    batch_size, negative_size = 8, 15
+
+    pred, succ = torch.randint(10, (2, batch_size))
+    negatives = torch.randint(10, (batch_size, negative_size))
+
+    model = EntailmentConesEmbeddings(10, 30, PoincareBall())
+
+    assert not torch.isnan(model(pred, succ, negatives))
+
+
+def test_entcon_lorentz_forward():
+    batch_size, negative_size = 8, 15
+
+    pred, succ = torch.randint(10, (2, batch_size))
+    negatives = torch.randint(10, (batch_size, negative_size))
+
+    model = EntailmentConesEmbeddings(10, 30, LorentzManifold())
+
+    assert not torch.isnan(model(pred, succ, negatives))
